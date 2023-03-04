646f2f772368ac8bc418db650833377204ae5491
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2023-03-04 19:39:23 +0100

    heroku config

--- sms/settings.py
+++ sms/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import sendgrid
-from sendgrid.helpers.mail import *
+# import sendgrid
+# from sendgrid.helpers.mail import *
 # from datetime import timedelta
 
 import dotenv
